e9f94028b2fa9244e47ecac8f75d7f69dad9742b
Nandu AY <nandu.a.y@hotmail.com>
2023-03-10 21:23:46 +0530

    Update 9th march

---
+++ Modules_Excercise.py
@@ -0,0 +1,9 @@
+#2 Methods to use Modules anf packets
+from Modules import divi    #**********************(1)
+import shopping.more_shopping.shopping_cart    #**********************(2)
+print(divi(10,2))
+
+print(shopping.more_shopping.shopping_cart.shop(100))
+
+
+

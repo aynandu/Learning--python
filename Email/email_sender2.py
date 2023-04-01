30d9d11d95f70ed554594cc9804cd43c016a8369
Nandu AY <nandu.a.y@hotmail.com>
2023-04-01 20:46:09 +0530

    Updated send Email - elearning

--- Email/email_sender2.py
+++ Email/email_sender2.py
@@ -1,12 +1,16 @@
-#send a Spam Mail to any Gmail ID using Python
+#send a Spam Mail (included HTML) to any Gmail ID using Python
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html=Template(Path('index.html').read_text())
 
 email = EmailMessage() #now email become the object
 email['Subject'] = 'You won 1,00,0000 dollars!'
 email['From'] = 'developerdemo@zohomail.in'
 email['To'] = 'nandukannanmala@hotmail.com'
-email.set_content('Iam a Python developer')
+email.set_content(html.substitute({'name':'Nandu','m':'Mr.'}),'html')
 
 with smtplib.SMTP('smtp.zoho.in', 587) as smtp:
     smtp.ehlo()
